a3ccf158a69b365a63f72d29d89ecb9bd65fcdf0
abdulazizcode <tajikcoder@gmail.com>
2021-08-26 00:19:23 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded
+
+
+//    Connecting to MongoDB
+mongoose
+    .connect('mongodb://localhost:27017/wikiDb', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('MongoDB Error');
+        console.log(err);
+    });
+
+
+//   Creating blueprint Schema
+
+const articlesSchema = {
+    title: String,
+    container: String
+};
+
+// Using our blueprint Schema
+
+const Article = mongoose.model('Article', articlesSchema);
+
+
+//    Getting all Posts
+
+app.get('/articles', (req, res) => {
+    Article.find({}, (err, foundedData) => {
+        if (!err) {
+            res.send(foundedData);
+        } else {
+            res.send('Error while fetching data', err);
+        }
+    });
+});
+
+
+//   Posting  new Post
+
+app.post('/articles', (req, res) => {
+    let newArticle = new Article({
+        title: req.body.title,
+        container: req.body.container
+    });
+
+    newArticle.save((err)=>{
+        if(!err){
+            res.send('Succesfully added new Post!')
+        }else{
+            res.send('Error occured while adding new Post!', err);
+        }
+    })
+});
+
+
+//   Deleteding all Posts
+
+app.delete('/articles', (req,res) =>{
+    Article.deleteMany({}, function (err) {
+        if(!err){
+            res.send('Succesfully deleted all Posts!')
+        }else{
+            res.send('Error occured while deleting all Posts!', err);
+        }
+    })
+})
+
+
+//    Listening to port 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
